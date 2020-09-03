709b9ebc0c22d7751c882d12886c8521f9e25900
Angel Acosta <Mrnurdee@gmail.com>
2020-09-03 12:30:27 -0700

    Add project files.

---
+++ Constructors/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Human
+    {
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I am {0} {1}. I have {2} eyes and {3} years old",firstName, lastName, eyeColor, age);
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human sissy = new Human("Sissy", "Wagner", "blue", 23);
+            sissy.IntroduceMyself();
+
+            Human angel = new Human("Angel", "Acosta", "brown", 23);
+            angel.IntroduceMyself();
+        }
+    }
+}
